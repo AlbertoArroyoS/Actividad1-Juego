3abbcc7b696867fef837d23d546a160521626f87
Alberto Arroyo <albertoarroyo@hotmail.es>
2023-10-05 13:48:17 +0200

    Funcionalidad obligatorio poner minimo 1 caractec nombre para activar boton aceptar

--- script/script.js
+++ script/script.js
@@ -1,14 +1,31 @@
 //declarar de las variables
-let name;
+let nameBox;
 let button;
+let optionSelected;
 
 //inicialización de las variables
 
+function initVariables(){
+    nameBox = document.getElementById("nombre");
+    button = document.getElementById("button");
+    nameBox.focus();
 
+}
 
 
 //uso de las variables
 
 window.addEventListener("load",()=>{
+
+
+    initVariables();
+
+    nameBox.addEventListener("input",()=>{
+		if(nameBox.value.length >= 1 && nameBox.value.length <= 25 ){
+			button.disabled = false; 
+		}else{
+			button.disabled = true; 
+		}
+	});
 
 });
