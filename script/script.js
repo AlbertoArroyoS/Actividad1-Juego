1a9f18d15f41667ef73174d211cf03d68c637032
Alberto Arroyo <albertoarroyo@hotmail.es>
2023-10-06 11:54:09 +0200

    Onclick de botones asignando valor, Prueba con matriz de resultados y dandole ese valor del click

--- script/script.js
+++ script/script.js
@@ -6,6 +6,19 @@ let caja2;
 let player1;
 let imagePulse;
 //inicialización de las variables
+// console.log("Valor de la imagen: " + valor);
+let botonJugada;
+
+let botonpiedra;
+let botonpapel;
+let botontijera;
+let botonlagarto;
+let botonspock;
+let resultadoBoton;
+
+let matrizResultados;
+let fraseResultados;
+let aleatorioMaquina;
 
 function initVariables(){
     nameBox = document.getElementById("nombre");
@@ -13,22 +26,114 @@ function initVariables(){
     nameBox.focus();
 	//player1.document.getElementsByName("jugador1")
 	// Obtén una NodeList de todas las imágenes con la clase "imagen"
-	imagePulse = document.querySelectorAll(".img-pulsada");
+	//imagePulse = document.querySelectorAll(".img-pulsada");
+	//botonJugada = document.getElementById("btnpiedra").value;
+	//console.log();
+	matrizResultados=[
+					[0,1,2,2,1],
+					[2,0,1,1,2],
+					[1,2,0,2,1],
+					[1,2,1,0,2],
+					[2,1,2,1,0]
+					];
+	fraseResultados = [
+					["Empate","Papel tapa piedra","Piedra rompe tijeras","Piedra aplasta lagarto","Spock vaporiza piedra"],
+					["Papel tapa piedra","Empate","tijeras cortan papel","lagarto devora papel","Papel desautoriza a Spock"],
+					["Piedra rompe tijeras","Tijeras cortan papel","Empate","Tijeras decapitan lagarto","Spock rompe tijeras"],
+					["Piedra aplasta lagarto","Lagarto devora papel","Tijeras decapitan lagarto","Empate","Lagarto envenena a Spock"],
+					["Spock vaporiza piedra","Papel desautoriza a Spock","Spock rompe tijeras","Lagarto envenena a Spock","Empate"]
+					];				
+	aleatorioMaquina= Math.floor(Math.random()*5);
+
+	botonpiedra= document.getElementById("btnpiedra");
+	botonpapel= document.getElementById("btnpapel");
+	botontijera= document.getElementById("btntijera");
+	botonlagarto= document.getElementById("btnlagarto");
+	botonspock= document.getElementById("btnspock");
+
+	
 }
 
 //funcion valor imagen pulsada
 
-function imgPulsada(){
-	// Itera sobre cada imagen y agrega un evento de clic a cada una
-	imagePulse.forEach(function(imagen) {
-		imagen.addEventListener("click", function() {
-		  // Obtiene el valor del atributo data-valor de la imagen actual
-		  var valor = imagen.getAttribute("data-valor");
+function empezarPartida(){
 	
-		  // Realiza la acción que desees con el valor obtenido, por ejemplo, mostrarlo en la consola
-		  console.log("Valor de la imagen: " + valor);
-		});
-	  });
+	botonpiedra.addEventListener("click",eligio_piedra);
+	botonpapel.addEventListener("click",eligio_papel);
+	botontijera.addEventListener("click",eligio_tijeras);
+	botonlagarto.addEventListener("click",eligio_lagarto);
+	botonspock.addEventListener("click",eligio_spock);
+	
+}
+
+
+function eligio_piedra(){
+	resultadoBoton=0;
+	resultadoPartida(resultadoBoton);
+}
+
+function eligio_papel(){
+	resultadoBoton=1;
+	resultadoPartida(resultadoBoton);
+}
+
+function eligio_tijeras(){
+	resultadoBoton=2;
+	resultadoPartida(resultadoBoton);
+}
+
+function eligio_lagarto(){
+	resultadoBoton=3;
+	resultadoPartida(resultadoBoton);
+}
+function eligio_spock(){
+	resultadoBoton=4;
+	resultadoPartida(resultadoBoton);
+}
+
+function resultadoPartida(numeroOpcion) {
+	//console.log(numeroOpcion);
+
+
+	
+
+}
+
+//añadir imagen grande a boton seleccionado
+function addImg (valor){
+	let miJugada;
+	let imagen = document.createElement("img");
+
+	switch (valor){
+		case 0:
+			imagen.src = "/icon/rock.png";
+			miJugada = document.getElementById("mi-jugada");
+			miJugada.appendChild(imagen);
+			break;
+
+		case 1:
+			imagen.src = "/icon/paper.png";
+			miJugada = document.getElementById("mi-jugada");
+			miJugada.appendChild(imagen);
+			break;
+
+		case 2:
+			imagen.src = "/icon/scissors.png";
+			miJugada = document.getElementById("mi-jugada");
+			miJugada.appendChild(imagen);
+			break;
+		case 3:
+			imagen.src = "/icon/lizard.png";
+			miJugada = document.getElementById("mi-jugada");
+			miJugada.appendChild(imagen);
+			break;
+		case 4:
+			imagen.src = "/icon/spock.png";
+			miJugada = document.getElementById("mi-jugada");
+			miJugada.appendChild(imagen);
+			break;
+	}
+
 }
 
 //uso de las variables
@@ -37,7 +142,7 @@ window.addEventListener("load",()=>{
 
 
     initVariables();
-	imgPulsada();
+	empezarPartida();
     nameBox.addEventListener("input",()=>{
 		if(nameBox.value.length >= 1 && nameBox.value.length <= 25 ){
 			button.disabled = false; 
